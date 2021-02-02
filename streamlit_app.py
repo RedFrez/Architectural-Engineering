fd1b587501b75f03d267c0faca261e9f8f102ae1
Christina Frezynksi <30352852+RedFrez@users.noreply.github.com>
2021-02-01 22:50:17 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+st.header('Adventures of Architectural Engineering')
+st.subheader('Coming Soon!)
